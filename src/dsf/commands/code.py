5a89b522a0e2c03ef4ebc57db0ec45834107e803
Loïc GRENON <loic.grenon@difoolou.net>
2023-08-19 17:00:53 +0200

    Add docstrings

--- src/dsf/commands/code.py
+++ src/dsf/commands/code.py
@@ -30,9 +30,20 @@ from ..object_model.messages import Message
 class CodeType(str, Enum):
     """Type of generic G/M/T-code. If none is applicable, it is treated as a comment"""
 
+    # Whole line comment
     Comment = "Q"
+
+    # Meta G-code keyword (not sent as a code to RRF)
+    # Codes of this type are not sent to RRF in binary representation
+    Keyword = "K"
+
+    # G-code
     GCode = "G"
+
+    # M-code
     MCode = "M"
+
+    # T-code
     TCode = "T"
 
 
