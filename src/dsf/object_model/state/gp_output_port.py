cb094d082d5b4e7ccf4565d9cf8825b9f45c1ebb
Loïc GRENON <loic.grenon@difoolou.net>
2023-08-21 16:55:36 +0200

    Added state.gpOut[].freq

--- src/dsf/object_model/state/gp_output_port.py
+++ src/dsf/object_model/state/gp_output_port.py
@@ -6,7 +6,19 @@ class GpOutputPort(ModelObject):
 
     def __init__(self):
         super(GpOutputPort, self).__init__()
+        # PWM frequency of this port (in Hz)
+        self._freq = 0
+        # PWM value of this port (0..1)
         self._pwm = 0
+
+    @property
+    def freq(self) -> int:
+        """PWM frequency of this port (in Hz)"""
+        return self._freq
+
+    @freq.setter
+    def freq(self, value):
+        self._freq = int(value)
 
     @property
     def pwm(self) -> float:
