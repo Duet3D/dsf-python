5fa381d22cfe9583f0a1ae8643281af66c470a6c
Loïc GRENON <loic.grenon@difoolou.net>
2023-08-22 14:23:37 +0200

    Changed direct-connect display OM definition

---
+++ src/dsf/object_model/boards/direct_display/__init__.py
@@ -0,0 +1,8 @@
+from .direct_display import DirectDisplay
+from .direct_display_controller import DirectDisplayController
+from .direct_display_encoder import DirectDisplayEncoder
+from .direct_display_screen import DirectDisplayScreen
+from .direct_display_screen_st7567 import DirectDisplayScreenST7567
+
+__all__ = ['DirectDisplay', 'DirectDisplayController', 'DirectDisplayEncoder',
+           'DirectDisplayScreen', 'DirectDisplayScreenST7567']
