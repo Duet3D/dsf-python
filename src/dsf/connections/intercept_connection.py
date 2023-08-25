4bfd191687b26b84f46f8a5b272c314da5710a5b
Loïc GRENON <loic.grenon@difoolou.net>
2023-08-25 18:02:05 +0200

    Added auto flush to code interceptor API

--- src/dsf/connections/intercept_connection.py
+++ src/dsf/connections/intercept_connection.py
@@ -1,8 +1,9 @@
-from typing import Optional
+from typing import List, Optional
 
 from .base_command_connection import BaseCommandConnection
 from .init_messages import client_init_messages
 from .. import commands, SOCKET_FILE
+from ..commands.code_channel import CodeChannel
 from ..object_model.messages import MessageType
 
 
@@ -12,23 +13,24 @@ class InterceptConnection(BaseCommandConnection):
     def __init__(
         self,
         interception_mode: client_init_messages.InterceptionMode,
-        channels=None,
-        filters=None,
+        channels: List[CodeChannel] = None,
+        filters: List[str] = None,
+        auto_flush: bool = True,
         priority_codes: bool = False,
         debug: bool = False,
     ):
         super().__init__(debug)
         self.interception_mode = interception_mode
-        self.channels = channels if channels is not None else commands.code_channel.CodeChannel.list()
+        self.channels = channels if channels is not None else CodeChannel.list()
         self.filters = filters
+        self.auto_flush = auto_flush
         self.priority_codes = priority_codes
 
-    def connect(self, socket_file: str = SOCKET_FILE):  # type: ignore
+    def connect(self, socket_file: str = SOCKET_FILE):  # noqa
         """Establishes a connection to the given UNIX socket file"""
         iim = client_init_messages.intercept_init_message(
-            self.interception_mode, self.channels, self.filters, self.priority_codes
+            self.interception_mode, self.channels, self.filters, self.priority_codes, self.auto_flush
         )
-
         return super().connect(iim, socket_file)
 
     def receive_code(self) -> commands.code.Code:
