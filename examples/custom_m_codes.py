d0673041d19bce328d2cabbaba84b84df851f849
Christian Hammacher <bmasterc@gmail.com>
2024-07-22 11:36:17 +0200

    Update custom_m_codes.py
    Updated comments to reflect better what DSF 3.5 does

--- examples/custom_m_codes.py
+++ examples/custom_m_codes.py
@@ -3,6 +3,7 @@
 Example of intercepting and interacting with codes
 
 Make sure when running this script to have access to the DSF UNIX socket owned by the dsf user.
+This is especially important when trying to run scripts as root user (which is not encouraged).
 """
 
 import subprocess
@@ -14,6 +15,10 @@ from dsf.object_model import MessageType
 
 
 def start_intercept():
+    # This demo script uses filters for the given three codes - M1234, M5678, M7722.
+    # If you intercept the whole G-code stream (i.e. no filter) and if you wish to wait for all other
+    # commands to be processed first, you MUST call flush() and wait for it to return true.
+    # In the following M7722 section, the flush() call isn't required but it serves as a basic example
     filters = ["M1234", "M5678", "M7722"]
     intercept_connection = InterceptConnection(InterceptionMode.PRE, filters=filters, debug=True)
     intercept_connection.connect()
@@ -25,20 +30,6 @@ def start_intercept():
 
             # Check for the type of the code
             if cde.type == CodeType.MCode and cde.majorNumber == 1234:
-                # --------------- BEGIN FLUSH ---------------------
-                # Flushing is only necessary if the action below needs to be in sync with the machine
-                # at this point in the GCode stream. Otherwise, it can and should be skipped
-
-                # Flush the code's channel to be sure we are being in sync with the machine
-                success = intercept_connection.flush(cde.channel)
-
-                # Flushing failed so we need to cancel our code
-                if not success:
-                    print("Flush failed")
-                    intercept_connection.cancel_code()
-                    continue
-                # -------------- END FLUSH ------------------------
-
                 # Do whatever needs to be done if this is the right code
                 print(cde, cde.flags)
 
@@ -50,6 +41,21 @@ def start_intercept():
                 # Exit this example
                 return
             elif cde.type == CodeType.MCode and cde.majorNumber == 7722:
+                # --------------- BEGIN FLUSH ---------------------
+                # Flushing is necessary if the action below needs to be in sync with the machine
+                # at this point in the GCode stream. If you only wish to inspect or log the G-code(s),
+                # it is not needed. It is not needed either if a filter is set, see above
+
+                # Flush the code's channel to be sure we are being in sync with the machine
+                success = intercept_connection.flush(cde.channel)
+
+                # Flushing failed so we need to cancel our code
+                if not success:
+                    print("Flush failed")
+                    intercept_connection.cancel_code()
+                    continue
+                # -------------- END FLUSH ------------------------
+
                 # We are going to shut down the SBC in one minute
                 subprocess.run(["sudo", "shutdown", "+1"])
                 # Resolve it with a custom response message text
