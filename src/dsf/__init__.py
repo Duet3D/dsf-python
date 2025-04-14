553d15a58a05cd30e354df2adffacfc757aec10a
AndyEveritt <38423143+AndyEveritt@users.noreply.github.com>
2025-04-14 10:32:28 +0100

    Get socket path from `/opt/dsf/conf/config.json`

--- src/dsf/__init__.py
+++ src/dsf/__init__.py
@@ -1,7 +1,22 @@
 __version__ = "3.6.0-rc1.post1"
 
-# path to unix socket file
+import json
+import os
+
+# Default socket file path
 SOCKET_FILE = "/run/dsf/dcs.sock"
+
+# Try to read socket file path from config
+config_path = "/opt/dsf/conf/config.json"
+if os.path.exists(config_path):
+    try:
+        with open(config_path, 'r') as f:
+            config = json.load(f)
+            socket_dir = config.get("SocketDirectory", "/run/dsf")
+            socket_file = config.get("SocketFile", "dcs.sock")
+            SOCKET_FILE = os.path.join(socket_dir, socket_file)
+    except (json.JSONDecodeError, IOError):
+        pass  # Use default if config file is invalid or inaccessible
 
 # allowed connection per unix server
 DEFAULT_BACKLOG = 4
